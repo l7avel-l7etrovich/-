a6f75a4f4808c42cac8e6aa064b5507bbf69311d
Pavel <Pavlik59@icloud.com>
2022-06-29 18:02:23 +0500

    Решили задачу 1

---
+++ Task_01/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Console.WriteLine("Эта программа проверяет, является ли первое число квадратом второго, Введите первое число и нажмите Enter"); 
+int first = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число и нажмите Enter");
+int second = Convert.ToInt32(Console.ReadLine());
+
+if (second * second == first)
+{
+    Console.WriteLine($"Да, первое число это квадрат второго: {second} * {second} = {first}");
+}
+else
+{
+    Console.WriteLine($"Нет, первое число не является квадратом второго: {second} * {second} = {first}");
+}
