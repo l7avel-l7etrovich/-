840adda6e66bd4768fe1508a5a369690baee869a
Pavel <Pavlik59@icloud.com>
2022-07-03 10:52:33 +0500

    Решили задачу 5

--- Task_05/Program.cs
+++ Task_05/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int negnum;
+if (num > 0) {
+negnum = num * -1;
+}
+else {
+negnum = num;
+num = negnum * -1;
+}
+while (negnum <= num) {
+Console.Write($" {negnum} ");
+negnum ++;
+}
