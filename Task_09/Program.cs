852c663d7e8cf25bdf999417e9b165bef0c3b2ed
Pavel <Pavlik59@icloud.com>
2022-07-06 20:04:05 +0500

    09

--- Task_09/Program.cs
+++ Task_09/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Эта программа выводит случайное число из отрезка [10, 99] и показавывает наибольшую цифру числа!");
+
+int randomNum = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число 10 - 99 => {randomNum}");
+
+//int firstDigit = randomNum / 10;
+//int secondDigit = randomNum % 10;
+//int maxDigit = firstDigit> secondDigit? firstDigit : secondDigit;
+//if (firstDigit > secondDigit) maxDigit = firstDigit;
+//else maxDigit = secondDigit;
+//Console.WriteLine($"Наибольшая цифра числа {randomNum} => {maxDigit}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    if (firstDigit == secondDigit) return 0;
+    return firstDigit> secondDigit? firstDigit : secondDigit;
+}
+
+int maxDigit = MaxDigit(randomNum);
+string result = maxDigit > 0 ? maxDigit.ToString() : "Цифры одинаковые";
+Console.WriteLine($"Найбольшая цифра числа {randomNum} => {result}");
