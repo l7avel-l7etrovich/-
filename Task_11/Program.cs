6b5199b2c952bab84cd761c2c23640884b6770c0
Pavel <Pavlik59@icloud.com>
2022-07-08 17:05:46 +0500

    Решил задачу 11

--- Task_11/Program.cs
+++ Task_11/Program.cs
@@ -1,4 +1,21 @@
-﻿int num = new Random().Next(100, 1000);
-Console.WriteLine(num);
-int result= ((num / 100) * 10) + (num % 10);
-Console.WriteLine(result);
+﻿// 11. Напишите программу, которая выводит
+// случайное трёхзначное число и удаляет
+// вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+var rndNum = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число: {rndNum}");
+
+int CutMid (int num)
+{
+int firstDig = num / 100 * 10;
+int secondDig = num % 10;
+return firstDig + secondDig;
+}
+int result = CutMid(rndNum);
+
+Console.WriteLine($"Полученное двухзначное число(через метод): {result}");
+
+Console.WriteLine($"Полученное двухзначное число: {rndNum / 100 * 10 + rndNum % 10}");
