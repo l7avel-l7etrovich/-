22b2cba1442e395cf3c411d23b5f6fe954f26e88
Pavel <Pavlik59@icloud.com>
2022-07-24 18:15:01 +0500

    Решил задачу 4

---
+++ Task_46/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+
+        }
+    }
+    return array;
+
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write(array[i, j] + ",");
+            else Console.Write(array[i, j] + "]");
+
+        }
+        System.Console.WriteLine();
+    }
+}
+int[,] arrayResult = CreateMatrixRndInt(3, 4, 10, 30);
+PrintMatrix(arrayResult);
